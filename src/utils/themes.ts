6039993c70566768589719c64851b4f4d11d852a
Marsad Ghanvi <marsad11223@gmail.com>
2025-04-07 16:59:47 +0500

    first commit

---
+++ src/utils/themes.ts
@@ -0,0 +1,39 @@
+// import { localFontSize } from "@/app/utils/themes";
+export const localColors = {
+  sideColor: "#111111",
+};
+
+export const localFontSize = {
+  // headings
+  h1: {
+    xs: "32px",
+    sm: "40px",
+    md: "50px",
+    lg: "64px",
+  },
+  h2: {
+    xs: "28px",
+    sm: "36px",
+    md: "44px",
+    lg: "48px",
+  },
+  h3: {
+    xs: "24px",
+    sm: "26px",
+    md: "28px",
+    lg: "32px",
+  },
+
+  h4: {
+    xs: "18px",
+    sm: "22px",
+    md: "26px",
+    lg: "28px",
+  },
+  // textual
+  p1: { xs: "18px", md: "22px", lg: "24px" },
+  p2: { xs: "14px", md: "16px", lg: "18px" },
+  p3: { xs: "14px", lg: "16px" },
+  p4: { xs: "12px", lg: "14px" },
+  p5: "12px",
+};
