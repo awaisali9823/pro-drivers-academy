65895b0bf87d042b7056c3f3f3ac82cb738de9c6
Marsad Ghanvi <marsad11223@gmail.com>
2025-04-08 13:10:12 +0500

    story section changed

---
+++ src/_assets/pngs/index.tsx
@@ -0,0 +1,10 @@
+import storycardImage1 from "@/_assets/pngs/storyImage1.png";
+import storycardImage2 from "@/_assets/pngs/storyImage2.png";
+import storycardImage3 from "@/_assets/pngs/storyImage3.png";
+const pngs = {
+  storycardImage1,
+  storycardImage2,
+  storycardImage3,
+};
+
+export default pngs;
