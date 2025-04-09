62903abbcc8b42e12f256755aad2a27b6db6a20f
Marsad Ghanvi <marsad11223@gmail.com>
2025-04-09 14:14:59 +0500

    changed in tabs

--- src/app/plans/component/ManualTab.tsx
+++ src/app/plans/component/ManualTab.tsx
@@ -71,7 +71,6 @@ export default function Manualtab() {
             xs: "center",
             md: "space-evenly",
             lg: "space-between",
-            xl: "space-around",
           },
           alignItems: "center",
         }}>
