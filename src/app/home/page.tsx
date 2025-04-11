695e4108c2c0dca2559fe963aa8a5595feecad73
Marsad Ghanvi <marsad11223@gmail.com>
2025-04-11 16:09:06 +0500

    build issues

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,9 +6,9 @@ import PopularCourses from "./_sections/PopularCourses";
 import NewLocation from "./_sections/NewLocation";
 import ContactUs from "@/_components/ContactUs";
 import Footer from "@/_components/Footer";
-import Tab from "@/_components/Tab";
-import Manualtab from "../plans/_components/ManualTab";
-import AutomaticTab from "../plans/_components/AutomaticTab";
+// import Tab from "@/_components/Tab";
+// import Manualtab from "../plans/_components/ManualTab";
+// import AutomaticTab from "../plans/_components/AutomaticTab";
 import OurProudGraduates from "./_sections/OurProudGraduates";
 
 export default function HomePage() {
